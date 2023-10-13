5dcf6fb8587c6c556c6127e832baa40245761a05
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-10-13 16:33:32 +0530

    Student form done

--- src/Page/AddStudent.jsx
+++ src/Page/AddStudent.jsx
@@ -11,37 +11,83 @@ export default function AddStudent() {
       <SideBar />
       <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginTop:"55px"}}>
         
-      <Typography variant='h4'>
+      <Typography variant='h6'>
+        <div className="center">
       <form className="form">
-  <p className="title">Register </p>
-  <p className="message">Signup now and get full access to our app. </p>
+  <p className="title">Student form </p>
+  {/* <p className="message">Student Registration form </p> */}
   <div className="flex">
-    <label>
-      <input className="input" type="text" placeholder="" required="" />
-      <span>Firstname</span>
+    <label>Full name :
+      <input required="" placeholder="full name" type="text" className="input" />
+      
     </label>
-    <label>
-      <input className="input" type="text" placeholder="" required="" />
+    {/* <label>
+      <input required="" placeholder="" type="text" className="input" />
       <span>Lastname</span>
-    </label>
+    </label> */}
   </div>
-  <label>
-    <input className="input" type="email" placeholder="" required="" />
-    <span>Email</span>
+  <div>
+    <label>Mobile no:
+    <input required="" placeholder="+91-XXXXXXXXXX" type="text" className="input" maxLength={13} />
+    {/* <span></span> */}
   </label>
-  <label>
-    <input className="input" type="password" placeholder="" required="" />
-    <span>Password</span>
+  </div>
+  <div>
+  <label>Email:
+    <input required="" placeholder="xyz@gmail.com" type="email" className="input" />
+    {/* <span></span> */}
   </label>
-  <label>
-    <input className="input" type="password" placeholder="" required="" />
-    <span>Confirm password</span>
+  </div>
+  <div>
+  <label>DOB(Date of Birth):
+    <input required="" placeholder="DOB" type="date" className="input" />
+    {/* <span></span> */}
   </label>
-  <button className="submit">Submit</button>
-  <p className="signin">
-    Already have an acount ? <a href="#">Signin</a>{" "}
-  </p>
+  </div>
+  <div>
+    <label>City:
+    <input required="" placeholder="City" type="text" className="input" maxLength={15} />
+    {/* <span></span> */}
+  </label>
+  </div>
+  <div>
+    <label>Address:
+    <input required="" placeholder="Address" type="text" className="input" maxLength={80} />
+    {/* <span></span> */}
+  </label>
+  </div>
+  <br />
+  <div>
+  <label>Select Course:
+  <select>
+  <option value="" defaultChecked>-SELECT-</option>
+    <option value="B.tech">B.tech</option>
+    <option value="Bio-Tech">Bio-Tech</option>
+    <option value="BBA">BBA</option>
+    <option value="BCA">BCA</option>
+    <option value="MBA">MBA</option>
+    <option value="MCA">MCA</option>
+    <option value="LLB">LLB</option>
+    <option value="MBBS">MBBS</option>
+  </select>
+  </label>
+
+  </div>
+ <br />
+  <div>
+    <input type="button" className="submit" value={"submit"} />
+    
+  </div>
+<br />
+  <div>
+    <input type="button" className="submit" value={"reset"} />
+    </div>
+  
+  
+
 </form>
+</div>
+
 
         </Typography>
       </Box>
