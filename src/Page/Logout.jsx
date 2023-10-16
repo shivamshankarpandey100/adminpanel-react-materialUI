13c56f9e7bba0c7c482fd41719abb529566cefc8
Aniket Nigam <aniknigam2@gmail.com>
2023-10-16 20:18:41 +0530

    logout and delete operation working

--- src/Page/Logout.jsx
+++ src/Page/Logout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SideBar from '../Component/SideBar'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import { useDispatch } from 'react-redux';
+import { signOutAsync } from '../features/auth/authSlice';
 
 export default function Logout() {
+ 
+  const dispatch =useDispatch()
+  useEffect(() => {
+    dispatch(signOutAsync())
+  }, [dispatch]);
   return (
     <Box sx={{display:'flex'}}>
       <SideBar />
