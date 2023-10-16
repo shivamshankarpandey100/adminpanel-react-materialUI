97299031f9258cb62b74dcdcb1b7b9073400cf75
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-10-16 13:37:12 +0530

    sab ho gya

--- src/Page/CourseList.jsx
+++ src/Page/CourseList.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
+
+
+import React, { useEffect, useState } from 'react';
 import SideBar from '../Component/SideBar'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import "./css/List.css"
 
 export default function CoursesList() {
+  const [column, setColumn] = useState([]);
+  const [records, setRecords] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3001/db')
+      .then((res) => res.json())
+      .then((data) => {
+        setColumn(Object.keys(data.course[0]));
+        setRecords(data.course);
+      });
+  }, []);
   return (
     <Box sx={{display:'flex'}}>
       <SideBar />
       <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginTop:"55px"}}>
-            <Typography variant="h5">
-             Course List
+            <Typography variant="">
+             <div className='heading'>Course List</div>
+             <div>
+            <table className="table">
+              <thead>
+                <tr>
+                  {column.map((c, i) => (
+                    <th key={i}>{c}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {records.map((record, i) => (
+                  <tr key={i}>
+                    <td>{record.ID}</td>
+                    <td>{record.CourseId}</td>
+                    <td>{record.CourseName}</td>
+                    <td>{record.CourseDescription}</td>
+                    <td>
+                      <button className='btn1'>Update</button>
+                      <button className='btn2'>Remove</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
             </Typography>
           
           </Box>
     </Box>
   )
 }
+
+
+
--- src/Page/StudentList.jsx
+++ src/Page/StudentList.jsx
@@ -1,29 +1,55 @@
-import React, { useEffect } from 'react'
-import SideBar from '../Component/SideBar'
+import React, { useEffect, useState } from 'react';
+import SideBar from '../Component/SideBar';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchStudentsAsync, selectStudents } from '../features/student/studentSlice';
-
-
+import "./css/List.css"
 
 export default function StudentList() {
-  const dispatch=useDispatch();
-  const studentList=useSelector(selectStudents);
-  console.log(studentList);
+  const [column, setColumn] = useState([]);
+  const [records, setRecords] = useState([]);
+
   useEffect(() => {
-   dispatch(fetchStudentsAsync());
-  }, [dispatch])
-  
+    fetch('http://localhost:3001/db')
+      .then((res) => res.json())
+      .then((data) => {
+        setColumn(Object.keys(data.users[0]));
+        setRecords(data.users);
+      });
+  }, []);
   return (
-    <Box sx={{display:'flex'}}>
+    <Box sx={{ display: 'flex' }}>
       <SideBar />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginTop:"55px"}}>
-            <Typography variant="h5">
-             Student List
-            </Typography>
-          
-          </Box>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: '55px' }}>
+        <Typography variant="">
+        <div className='heading'>Student List</div>
+          <div>
+            <table className="table">
+              <thead>
+                <tr>
+                  {column.map((c, i) => (
+                    <th key={i}>{c}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {records.map((record, i) => (
+                  <tr key={i}>
+                    <td>{record.ID}</td>
+                    <td>{record.Name}</td>
+                    <td>{record.Email}</td>
+                    <td>{record.Password}</td>
+                    <td>
+                      <button className='btn1'>Update</button>
+                      <button className='btn2'>Remove</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </Typography>
+      </Box>
     </Box>
-  )
+  );
 }
+
