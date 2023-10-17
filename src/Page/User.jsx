ae4a24f8206e7b2dfab24347ec0569e44544a57e
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-10-17 23:09:01 +0530

    user Profile card done validation held

--- src/Page/User.jsx
+++ src/Page/User.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import SideBar from '../Component/SideBar'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import img from "./image/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
+import "./css/user.css"
+
 
 
 export default function User() {
@@ -11,6 +14,22 @@ export default function User() {
           <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginTop:"55px"}}>
             <Typography variant="h5">
              User
+             <p className="title">Current User</p>
+             <div>
+             
+  <h2>User Profile</h2>
+  <div className="card">
+    <img src={img} alt="John" className='card2' />
+    <h6>user name</h6>
+    <h6>Email</h6>
+    
+    <p>Admin User</p>
+    <div style={{ margin: "24px 0" }}>
+      
+    </div>
+  </div>
+
+             </div>
             </Typography>
           </Box>
         </Box>
