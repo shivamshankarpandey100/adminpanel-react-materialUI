130dc4e7218bf1ecd83aad4b6b109d619dc09b35
Zaid <mohmmadzaid759@gmail.com>
2023-10-20 18:50:25 +0530

    Our team data added

--- src/Page/Dashboard.jsx
+++ src/Page/Dashboard.jsx
@@ -1,45 +1,74 @@
-import React from 'react'
-import SideBar from '../Component/SideBar'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import img from "./image/ESD_LOGO_Rectangular.png"
-import "./css/dbrd.css"
-
+import React from "react";
+import SideBar from "../Component/SideBar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import "./css/dbrd.css";
 
 export default function Dashboard() {
   return (
-    <Box sx={{display:'flex'}}>
+    <Box sx={{ display: "flex" }}>
       <SideBar />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginTop:"55px"}}>
-        <div className='bgi'>
-        <Typography variant='#'>
-          
-            
-          <img src={img} alt="user" className='esd' />
-          <h1><u>ESD (Employee Skill's Devlopment Program) :</u></h1>
-          
-          <b><h3>That is the Admin's Panel Dashboard</h3></b>
-      </Typography>
-      <Typography paragraph>
-        <span className='spn'>
-        ESD program is for the Student to Devloped him/her skills.
-       Employee skill development is a crucial aspect of employee training and development programs.
-        Upskilling and reskilling employees is the primary focus of L&D programs in 2023, 
-        as it helps workers meet organizational standards, reduces replacement costs, 
-        and improves workplace performance.
-         Employee training programs help employees learn specific knowledge or 
-         skills to improve performance in their current roles, while employee 
-         development focuses on employee growth and future performance, 
-         rather than an immediate job role. High-impact training and development
-          programs don't just happen, and managers and employees work together to
-           create a development plan that identifies areas to develop or enhance 
-           and ascertains what actions or activities need to be taken to acquire and embed that learning.
-        </span>
-     
-      </Typography>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
+        <div className="bgi">
+          <Typography variant="">
+            <>
+              <h1>
+                <u>ESD (Employee Skill's Devlopment Program) :</u>
+              </h1>
+
+              <b>
+                <h3 className="our-team-heading">Our Team</h3>
+              </b>
+              <div className="dashboard-flex-container">
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image" id="shivam" />
+                  <h2 className="dashboard-h2">Shivam Shankar Pandey</h2>
+                  <h3 className="dashboard-h3">Front-End Development</h3>
+                </div>
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image" id="zaid"/>
+                  <h2 className="dashboard-h2">Mohammad Zaid</h2>
+                  <h3 className="dashboard-h3">Front-End Development</h3>
+                </div>
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image"  id="aniket"/>
+                  <h2 className="dashboard-h2">Aniket Nigam</h2>
+                  <h3 className="dashboard-h3">Back-End Development</h3>
+                </div>
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image" id="rahil" />
+                  <h2 className="dashboard-h2">Mohammad Rahil</h2>
+                  <h3 className="dashboard-h3">Back-End Development</h3>
+                </div>
+                <div className="dashboard-flex-item" >
+                  <div className="dashboard-round-image" id="prateek"/>
+                  <h2 className="dashboard-h2">Prateek Kumar</h2>
+                  <h3 className="dashboard-h3">Software Testing</h3>
+                </div>
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image" id="ashwariya" />
+                  <h2 className="dashboard-h2">Aishwariya Singh</h2>
+                  <h3 className="dashboard-h3">Software Testing</h3>
+                </div>
+              </div>
+              <div className="dashboard-center-items">
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image" id="aryan"></div>
+                  <h2 className="dashboard-h2">Database Administration</h2>
+                  <h3 className="dashboard-h3">Aryan Dwivedi</h3>
+                
+                </div>
+                <div className="dashboard-flex-item">
+                  <div className="dashboard-round-image" id="amisha"></div>
+                  <h2 className="dashboard-h2">Database Administration</h2>
+                  <h3 className="dashboard-h3">Amisha Kumar</h3>
+                </div>
+              </div>
+              <footer className="dashboard-footer">Copyright ESD 2023</footer>
+            </>
+          </Typography>
         </div>
-       
       </Box>
     </Box>
-  )
+  );
 }
